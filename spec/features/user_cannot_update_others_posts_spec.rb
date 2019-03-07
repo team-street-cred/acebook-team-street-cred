d7d0c037dec0b3dd1242a7e6fd528b017eaae42e
david-mears <kieukmail@gmail.com>
2019-03-07 12:56:48 +0000

    Wrote failing test

--- spec/features/user_cannot_update_others_posts_spec.rb
+++ spec/features/user_cannot_update_others_posts_spec.rb
@@ -4,15 +4,11 @@ require 'web_helper'
 RSpec.feature "Not update posts", type: :feature do
   scenario "User cannot update others' posts" do
     log_in
-    expect(current_user.first_name).to eq('Bob')
     click_link "New post"
     fill_in "Message", with: "I'm going to update this one"
     click_button "Submit"
     expect(page.find('#posts li:first-child')).to have_content("I'm going to update this one")
-    page.find('#posts li:first-child').click_button('Edit')
-    fill_in "post_message", with: "This is updated"
-
-
+    expect{page.find('#posts li:first-child').click_button('Edit')}.to raise_error(Capybara::ElementNotFound)
   end
 
   scenario "User can update own posts" do
