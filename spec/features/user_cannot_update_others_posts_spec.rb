1d33fc7e96aaf79301826f34dea11c7b7be90ff1
david-mears <kieukmail@gmail.com>
2019-03-07 13:09:12 +0000

    users can only update their own posts - button hidden

---
+++ spec/features/user_cannot_update_others_posts_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'web_helper'
+
+RSpec.feature "Not update posts", type: :feature do
+  scenario "User cannot update others' posts" do
+    log_in
+    expect(page.find('#posts li:nth-child(2)')).to have_content("Huzzay!")
+    expect{page.find('#posts li:nth-child(2)').click_button('Edit')}.to raise_error(Capybara::ElementNotFound)
+  end
+
+  scenario "User can update own posts" do
+    log_in
+    click_link "new post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+    page.find('#posts li:first-child').click_button('Edit')
+    fill_in "post_message", with: "This is updated"
+    click_button "Submit"
+    expect(page).to have_content("This is updated")
+  end
+end
