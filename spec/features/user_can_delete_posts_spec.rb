3e66e0989e45ebfc8f4bb71b9f5319bb5bf4a283
Erin Stevenson <estevenson_94@live.com>
2019-03-10 11:25:14 +0000

    Change Destroy to Delete in tests

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -2,7 +2,7 @@ feature 'Users can delete posts', type: :feature do
 
   scenario 'Users can delete posts' do
     log_in_and_go_to_posts
-    expect { click_link('Destroy', match: :first) }.to change(Post, :count).by(-1)
+    expect { click_link('Delete', match: :first) }.to change(Post, :count).by(-1)
     expect(page).to have_content 'Post was successfully destroyed.'
   end
 
@@ -10,7 +10,7 @@ feature 'Users can delete posts', type: :feature do
     sign_up
     visit "/posts"
     expect(page).to have_content('acebook')
-    expect { click_link('Destroy', match: :first) }.to_not change(Post, :count)
+    expect { click_link('Delete', match: :first) }.to_not change(Post, :count)
     expect(page).to have_content('You do not have permission to delete this post.')
   end
 end
