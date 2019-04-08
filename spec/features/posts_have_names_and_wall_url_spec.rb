e6baeef996786a0264ebdc4be6d326b99f063af7
Erin Stevenson <estevenson_94@live.com>
2019-04-08 12:01:28 +0100

    :sparkles: Users can log out
    Co-authored-by: Colin Hulme <17675453+ch359@users.noreply.github.com>

--- spec/features/posts_have_names_and_wall_url_spec.rb
+++ spec/features/posts_have_names_and_wall_url_spec.rb
@@ -2,7 +2,7 @@ RSpec.feature 'Posts have name and wall url' do
   scenario 'can click on the name of user who created post and be redirected to wall ' do
     log_in
     expect(page).to have_link("new post")
-    find("ul li:nth-child(1)").click_link("Jane")
+    find("ul#posts li:nth-child(1)").click_link("Jane")
     expect(page).to have_selector('h1', text: "Jane's wall")
   end
 end
